f86d38ee928ced5c74ea501bec6533b71743fa76
ebnn <partialsum@hotmail.com>
2014-07-13 21:07:33 +1000

    Add benchmark comparison with STL list

--- benchmarks/queue.cpp
+++ benchmarks/queue.cpp
@@ -3,12 +3,13 @@
 #include "retro/queue.hpp"
 
 #include <queue>
+#include <list>
 
 BENCHMARK(PartialQueue, PushEmptyStrings, 1, 10)
 {
   retro::partial_queue<std::string> q;
 
-  for (int i = 0; i < 10000; i++)
+  for (int i = 0; i < 100000; i++)
     q.push("");
 }
 
@@ -16,6 +17,14 @@ BENCHMARK(StlQueue, PushEmptyStrings, 1, 10)
 {
   std::queue<std::string> q;
 
-  for (int i = 0; i < 10000; i++)
+  for (int i = 0; i < 100000; i++)
     q.push("");
+}
+
+BENCHMARK(StlList, PushEmptyStrings, 1, 10)
+{
+  std::list<std::string> q;
+
+  for (int i = 0; i < 100000; i++)
+    q.push_back("");
 }

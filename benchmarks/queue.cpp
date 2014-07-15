8d1f3ea40624d4ea815eba4af723ec6ce7cf9376
ebnn <partialsum@hotmail.com>
2014-07-15 13:00:15 +1000

    Add push pop benchmark test

--- benchmarks/queue.cpp
+++ benchmarks/queue.cpp
@@ -28,3 +28,42 @@ BENCHMARK(StlList, PushEmptyStrings, 1, 10)
   for (int i = 0; i < 100000; i++)
     q.push_back("");
 }
+
+BENCHMARK(PartialQueue, PushPopEmptyStrings, 1, 10)
+{
+  retro::partial_queue<std::string> q;
+
+  for (int i = 0; i < 100000; i++)
+    q.push("");
+
+  while (!q.empty())
+  {
+    q.pop();
+  }
+}
+
+BENCHMARK(StlQueue, PushPopEmptyStrings, 1, 10)
+{
+  std::queue<std::string> q;
+
+  for (int i = 0; i < 100000; i++)
+    q.push("");
+
+  while (!q.empty())
+  {
+    q.pop();
+  }
+}
+
+BENCHMARK(StlList, PushPopEmptyStrings, 1, 10)
+{
+  std::list<std::string> q;
+
+  for (int i = 0; i < 100000; i++)
+    q.push_back("");
+
+  while (!q.empty())
+  {
+    q.pop_back();
+  }
+}

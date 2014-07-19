cf710c7b87d766594512761b734eee873e96e6d4
ebnn <partialsum@hotmail.com>
2014-07-19 15:02:40 +1000

    Fix unsigned comparison warnings in queue tests

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -2,28 +2,22 @@
 
 #include "retro/queue.hpp"
 
-TEST(partial_queue, emptyWhenInitiallyCreated)
-{
-  retro::partial_queue<int> q;
-  ASSERT_EQ(0, q.size());
-}
-
 TEST(partial_queue, pushingElementsDoesNotChangeFrontButChangeBack)
 {
   retro::partial_queue<int> q;
 
   q.push(1);
-  ASSERT_EQ(1, q.size());
+  ASSERT_EQ(1U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(1, q.back());
 
   q.push(2);
-  ASSERT_EQ(2, q.size());
+  ASSERT_EQ(2U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(2, q.back());
 
   q.push(3);
-  ASSERT_EQ(3, q.size());
+  ASSERT_EQ(3U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(3, q.back());
 }
@@ -35,22 +29,22 @@ TEST(partial_queue, poppingElementsGivesCorrectFrontAndBack)
   q.push(1);
   q.push(2);
   q.push(3);
-  ASSERT_EQ(3, q.size());
+  ASSERT_EQ(3U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(3, q.back());
 
   q.pop();
-  ASSERT_EQ(2, q.size());
+  ASSERT_EQ(2U, q.size());
   EXPECT_EQ(2, q.front());
   EXPECT_EQ(3, q.back());
 
   q.pop();
-  ASSERT_EQ(1, q.size());
+  ASSERT_EQ(1U, q.size());
   EXPECT_EQ(3, q.front());
   EXPECT_EQ(3, q.back());
 
   q.pop();
-  ASSERT_EQ(0, q.size());
+  ASSERT_EQ(0U, q.size());
 }
 
 TEST(partial_queue, pushInThePastGivesCorrectFrontAndBack)
@@ -59,25 +53,25 @@ TEST(partial_queue, pushInThePastGivesCorrectFrontAndBack)
 
   // queue: [3]
   auto t3 = q.push(3);
-  ASSERT_EQ(1, q.size());
+  ASSERT_EQ(1U, q.size());
   EXPECT_EQ(3, q.front());
   EXPECT_EQ(3, q.back());
 
   // queue: [2, 3]
   auto t2 = q.push(t3, 2);
-  ASSERT_EQ(2, q.size());
+  ASSERT_EQ(2U, q.size());
   EXPECT_EQ(2, q.front());
   EXPECT_EQ(3, q.back());
 
   // queue: [1, 2, 3]
   q.push(t2, 1);
-  ASSERT_EQ(3, q.size());
+  ASSERT_EQ(3U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(3, q.back());
 
   // queue: [1, 4, 2, 3]
   q.push(t2, 4);
-  ASSERT_EQ(4, q.size());
+  ASSERT_EQ(4U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(3, q.back());
 }
@@ -91,31 +85,31 @@ TEST(partial_queue, revertPushGivesCorrectFrontAndBack)
   auto t2 = q.push(2);
   auto t3 = q.push(3);
   auto t4 = q.push(4);
-  ASSERT_EQ(4, q.size());
+  ASSERT_EQ(4U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(4, q.back());
 
   // queue: [1, 3, 4]
   q.revert(t2);
-  ASSERT_EQ(3, q.size());
+  ASSERT_EQ(3U, q.size());
   EXPECT_EQ(1, q.front());
   EXPECT_EQ(4, q.back());
 
   // queue: [3, 4]
   q.revert(t1);
-  ASSERT_EQ(2, q.size());
+  ASSERT_EQ(2U, q.size());
   EXPECT_EQ(3, q.front());
   EXPECT_EQ(4, q.back());
 
   // queue: [3]
   q.revert(t4);
-  ASSERT_EQ(1, q.size());
+  ASSERT_EQ(1U, q.size());
   EXPECT_EQ(3, q.front());
   EXPECT_EQ(3, q.back());
 
   // queue: []
   q.revert(t3);
-  ASSERT_EQ(0, q.size());
+  ASSERT_EQ(0U, q.size());
 }
 
 TEST(partial_queue, moveIsEquivalentToPush)
@@ -128,7 +122,7 @@ TEST(partial_queue, moveIsEquivalentToPush)
   q.push(std::move(p1));
   q.push(std::move(p2));
 
-  ASSERT_EQ(2, q.size());
+  ASSERT_EQ(2U, q.size());
   EXPECT_EQ(1, q.front().first);
   EXPECT_EQ(2, q.front().second);
   EXPECT_EQ(3, q.back().first);
